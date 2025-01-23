e444086d3f24ae6bc769e46ba94f9c37430bb95c
Vibgans <160671564+Vibgans@users.noreply.github.com>
2025-01-22 20:02:19 -0800

    nothing

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -68,6 +68,7 @@ public class Intake extends SubsystemBase {
             intakeActuation.set(0);
         }
     }
+
     public void intakeCoral(double speed) {
         intakeWheels.set(speed);
     }
